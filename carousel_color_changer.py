17eb8a29aac52bb7a4ac37e406129644d8a5863d
leonidimeev <leonidimeev@yandex.ru>
2023-06-19 02:56:50 +0900

    set few tokens to carousel

--- carousel_color_changer.py
+++ carousel_color_changer.py
@@ -5,22 +5,22 @@ import os
 
 url = "https://discord.com/api/v9/guilds/355164847652077568/roles/1120033388842192997"
 # Retrieve the value of the 'TOKEN' environment variable
-token = os.getenv('AUTHORIZATION_TOKEN')
-
-headers = {
-    'authority': 'discord.com',
-    'authorization': f'{token}',
-    'accept': '*/*',
-    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-    'content-type': 'application/json'
-}
+array_string = os.getenv('AUTHORIZATION_TOKENS')
+tokens_array = array_string.split(',')
+using_token_enumerator = 0
 
 colors = [1752220, 1000220, 1750020, 2752220]
 color_enumerator = 0
 
-
-
 while True:
+    headers = {
+        'authority': 'discord.com',
+        'authorization': f'{tokens_array[using_token_enumerator]}',
+        'accept': '*/*',
+        'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        'content-type': 'application/json'
+    }
+
     # Color changer
     color_change_payload = json.dumps({
         "name": "Carousel",
@@ -40,5 +40,11 @@ while True:
     if color_enumerator == len(colors):
         color_enumerator = 0
 
+    if color_change_response.status_code == 429:
+        using_token_enumerator += 1
+        if using_token_enumerator == len(tokens_array):
+            using_token_enumerator = 0
+        print(f'Changed to using token: {using_token_enumerator}')
+
     # Delay
-    time.sleep(0.2)
+    time.sleep(0.5)
